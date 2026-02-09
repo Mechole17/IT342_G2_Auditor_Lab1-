12315300f533e38d5f4bf639a67aedcc61c0e265
Mechole17 <232763660+Mechole17@users.noreply.github.com>
2026-02-09 23:42:52 +0800

    web(pages):landing and dashboard

--- web/userauth/src/App.js
+++ web/userauth/src/App.js
@@ -1,11 +1,18 @@
 import RegistrationPage from './pages/registrationpage';
+import LandingPage from './pages/landingpage';
+import Dashboard from './pages/dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <RegistrationPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
